e9b1cb0ceebe9c927223256a3f44ac967c7ac48e
itatarinov <yarcrazy2323@gmail.com>
2025-05-21 20:26:19 +0300

    update

--- test.php
+++ test.php
@@ -8,61 +8,29 @@
 	5. Разместите код в гите и пришлите ссылку
 */
 
-/**
- * Класс для работы с API
- *
- * @author		User Name
- * @version		v.1.0 (dd/mm/yyyy)
- */
-class Api
-{
-	public function __construct()
-	{
-	
-	}
-
-
-	/**
-	 * Заполняет строковый шаблон template данными из объекта object
-	 *
-	 * @author		User Name
-	 * @version		v.1.0 (dd/mm/yyyy)
-	 * @param		array $array
-	 * @param		string $template
-	 * @return		string
-	 */
-	public function get_api_path(array $array, string $template) : string
-	{
-		$result = '';
-
-		/* Здесь ваш код */
-
-		return $result;
-	}
-}
+require_once "Api.php";
 
 $user =
-[
-	'id'		=> 20,
-	'name'		=> 'John Dow',
-	'role'		=> 'QA',
-	'salary'	=> 100
-];
+    [
+        'id' => 20,
+        'name' => 'John Dow',
+        'role' => 'QA',
+        'salary' => 100
+    ];
 
 $api_path_templates =
-[
-	"/api/items/%id%/%name%",
-	"/api/items/%id%/%role%",
-	"/api/items/%id%/%salary%"
-];
+    [
+        "/api/items/%id%/%name%",
+        "/api/items/%id%/%role%",
+        "/api/items/%id%/%salary%"
+    ];
 
-$api = new Api();
+$api = new Api($user);
 
-$api_paths = array_map(function ($api_path_template) use ($api, $user)
-{
-	return $api->get_api_path($user, $api_path_template);
+$api_paths = array_map(function ($api_path_template) use ($api) {
+    return $api->getApiPath($api_path_template);
 }, $api_path_templates);
 
-echo json_encode($api_paths, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE););
+echo json_encode($api_paths, JSON_UNESCAPED_UNICODE);
 
-$expected_result = ['/api/items/20/John%20Dow','/api/items/20/QA','/api/items/20/100'];
+$expected_result = ['/api/items/20/John%20Dow', '/api/items/20/QA', '/api/items/20/100'];
